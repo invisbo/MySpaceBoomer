95d49a5155240fc6c162c483020b538ef7450942
Caner Aksoy <caksoy@invio.com>
2017-11-21 15:09:38 +0300

    First Commit

---
+++ MySpaceBoomer/Assets/Scripts/DestroByContact.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroByContact : MonoBehaviour {
+
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    private Controller controller;
+    public int ScoreValue;
+
+    void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if(gameControllerObject != null)
+        {
+            controller = gameControllerObject.GetComponent <Controller>();
+        } else
+        {
+            Debug.Log("gameControllerObject bot found");
+        }
+    }
+
+    // Use this for initialization
+    void OnTriggerEnter (Collider other) {
+        Debug.Log(other.name);
+        if(!other.tag.Equals("Boundry"))
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+            if (other.tag.Equals("Player"))
+            {
+                Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+                controller.GameOver();
+            }
+            controller.AddScore(ScoreValue);
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+		
+	}
+}
